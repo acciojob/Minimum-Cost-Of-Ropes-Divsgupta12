6d629e37dbc48af3be32050c6571f47e367d113b
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-02-23 17:01:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function minCostOfRopes(event){
+	let inputelement = document.querySelector("input").value;
+	let arr = inputelement.split(',');
+	arr.sort(function(a,b) {return a-b});
+	let cost = 0;
+	while(arr.length>1){
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.push(res);
+		cost += res;
+		arr.shift();
+		arr.shift();
+
+		arr.sort(function (a,b) {reurn a-b});
+		
+	}
+	document.getElementById("result").textContent = cost;
+}
